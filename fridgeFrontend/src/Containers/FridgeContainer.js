59beccd58e88261e1573ee61c35633bc24694b37
Carla022 <cs.morenol@outlook.com>
2022-10-26 20:16:01 +0100

    uncommented container

--- fridgeFrontend/src/Containers/FridgeContainer.js
+++ fridgeFrontend/src/Containers/FridgeContainer.js
@@ -1,26 +1,26 @@
-// import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
-// import ShoppingList from "../Components/ShoppingList";
-// import Login from "../Components/Login";
-// import FavouriteList from "../Components/FavouriteList";
-// import Fridge from "../Components/currentFridge";
-// import NavBar from "../Components/NavBar/NavBar";
-// import React, { useState,useEffect } from "react";
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import ShoppingList from "../Components/ShoppingList";
+import Login from "../Components/Login";
+import FavouriteList from "../Components/FavouriteList";
+import Fridge from "../Components/currentFridge";
+import NavBar from "../Components/NavBar/NavBar";
+import React, { useState,useEffect } from "react";
 
-// const FridgeContainer =() => {
+const FridgeContainer =() => {
 
-//     const [isShowAddItem, setIsShowAddItem] = useState(false);
-//     const handleAddItemClick =() => {
-//         setIsShowAddItem(!isShowAddItem);
-//     };
+    const [isShowAddItem, setIsShowAddItem] = useState(false);
+    const handleAddItemClick =() => {
+        setIsShowAddItem(!isShowAddItem);
+    };
 
-//     return(
+    return(
 
-//     <>
-//     <NavBar handleAddItemClick = {handleAddItemClick} isShownAddItem={isShowAddItem}  Fridge={Fridge}/>
+    <>
+    <NavBar handleAddItemClick = {handleAddItemClick} isShownAddItem={isShowAddItem}  Fridge={Fridge}/>
 
-//     </>
-//     )
+    </>
+    )
 
-// };
+};
 
-// export default FridgeContainer;
+export default FridgeContainer;
