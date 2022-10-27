b858dd03734714c4306bafccd1cb92dacab6b6d3
Vilte N <viltenor@gmail.com>
2022-10-27 09:55:06 +0100

    auth works

--- DigitalFridgeAPI/src/main/java/com/example/DigitalFridgeAPI/controllers/AuthController.java
+++ DigitalFridgeAPI/src/main/java/com/example/DigitalFridgeAPI/controllers/AuthController.java
@@ -81,8 +81,8 @@ public class AuthController {
 
         // Create new user's account
         User user = new User(signUpRequest.getUsername(),
-                signUpRequest.getEmail(),
-                encoder.encode(signUpRequest.getPassword()));
+                encoder.encode(signUpRequest.getPassword()),
+                signUpRequest.getEmail());
 
 
 
